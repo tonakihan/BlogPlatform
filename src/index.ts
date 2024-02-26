b38ae4fa01440c887ffe12b67c2b6693145d016b
tonakihan <tonakihan@gmail.com>
2024-02-26 14:42:22 +0900

    Add env to console

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,7 @@
 import express from "express";
 import type {Express, Request, Response} from "express";
 import router from "./router";
+import path from "path";
 //import { Pool } from "pg";
 
 const PORT = process.env.PORT || "3000";
@@ -11,18 +12,19 @@ const app: Express = express();
   password: process.env.POSTGRES_PASSWORD || '',
   database: process.env.POSTGRES_DATABASE_NAME,
 });*/
-
+app.use(express.static(path.basename('/build')));
 app.use("/api", router);
 
-app.get("/", (req: Request, res: Response) => {
+/*app.get("/", (req: Request, res: Response) => {
   res.send("Express works! ('v')");
 //  pool.query('SELECT * FROM users')
 //    .then(result => res.send(result));
-});
+});*/
 
 function main() {
   try {
     app.listen(PORT, () => {
+      console.log(process.env);
       console.log(
         `Server launch. Listen port ${PORT}\n` +
         `http://localhost:${PORT}`
