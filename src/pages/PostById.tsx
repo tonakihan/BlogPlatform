1241ca5e1e45ff90cffd8639593b43b67696e6b5
Mark <tonakihan@gmail.com>
2024-04-18 15:43:10 +0900

    Create page PostById

--- src/pages/PostById.tsx
+++ src/pages/PostById.tsx
@@ -1,23 +1,39 @@
-import type { FC } from "react";
+import { useEffect, type FC } from "react";
 import { useParams } from "react-router-dom";
 import Error from "../components/Error";
+import { useAppSelector } from "../hooks/redux/useAppSelector";
+import { useAppDispatch } from "../hooks/redux/useAppDispatch";
+import { fetchPosts } from "../store/thunk/postThunk";
+import Loading from "../components/Loading";
+import PostItem from "../components/PostItem";
 
 type PostByIdParams = {
   id: string;
 }
 
+//TODO: КОСТЫЛИЩЕ! Переделать под использование отдельной функции...
 const PostById: FC = () => {
   const { id } = useParams<PostByIdParams>();
-
   if (!id || isNaN(id)) 
     return (<Error message="Не могу понять id"/>);
-  
   const postId = parseInt(id, 10);
 
-  //TODO: реализовать поддержку со стороны redux и отрисовку компонента
-  return (
-    <div>ТИПО ПОСТ id = {postId}</div>
-  );
+  const { posts, isLoading, error } = useAppSelector( state => state.posts );
+  const dispatch = useAppDispatch();
+  
+  useEffect(() => {
+    dispatch(fetchPosts());
+  }, [dispatch]);
+
+  const post = posts.find(post => post.id == postId);
+
+  if (isLoading) 
+    return (<Loading/>);
+
+  if (!post || error)
+    return (<Error message={error ?? "Не найден post"}/>);
+
+  return (<PostItem post={post}/>);
 };
 
 export default PostById;
